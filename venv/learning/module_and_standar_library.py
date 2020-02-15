f9f115b237fc6252071c9e5ec8675a64a9a6a297
antocecere <antocecere77@gmail.com>
2020-02-15 16:51:15 +0100

    Added module and standard library example

---
+++ venv/learning/module_and_standar_library.py
@@ -0,0 +1,32 @@
+#I moduli
+#/lib/site-packages
+
+import script
+script.hello_world()
+
+#import geometry
+#from geometry import Triangle, Square
+
+import os
+print(os.getcwd())
+
+import datetime
+print(datetime.datetime.now())
+
+from time import time
+from math import pow as power
+print(time())
+
+n = 2
+pow = 10
+n_pow = 20
+
+tick = time()
+
+power(n, n_pow)
+
+for _ in range(pow):
+    n_pow=n
+
+duration = time() - tick
+print(duration)
