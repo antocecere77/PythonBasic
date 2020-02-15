4737291d19ea4a0aee455062b5ffe3613f860ede
antocecere <antocecere77@gmail.com>
2020-02-15 12:04:58 +0100

    Added input basic command

---
+++ venv/learning/input.py
@@ -0,0 +1,32 @@
+name = input("Come ti chiami? ")
+print(name)
+
+grandpa_age = input("Quanti anni ha tuo nonno? ")
+
+var = "ciao"
+type(var)
+
+var = 3.5
+type(var)
+print(var+3)
+
+var = False;
+type(var)
+print(var)
+
+#casting
+var = 5;
+
+var = str(5)
+type(var)
+print(var)
+
+var = "10"
+var = int(var)
+type(var)
+
+var = "10.5"
+var = float(var)
+type(var)
+
+var/2
