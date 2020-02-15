4c69792e644879e982733f95b5297b67848f8406
antocecere <antocecere77@gmail.com>
2020-02-15 11:52:59 +0100

    first commit

---
+++ venv/learning/inputoutput.py
@@ -0,0 +1,26 @@
+print("ciao python")
+print(5)
+print(2.3)
+#somma
+print(5+3)
+
+#sottrazione
+print(10-2.2)
+
+#moltiplicazione
+print(10*30)
+
+#divisione
+print(10/3)
+
+#divisione (solo intero)
+print(10//3)
+
+#rest
+print(10%3)
+
+#potenza
+print(10**2)
+
+#unire le stringhe
+print("5"+"2")
